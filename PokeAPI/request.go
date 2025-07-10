c624a9a6166c136469ec61a7a56b540de2db4eed
Saif Siddiqui <saifullah.siddiqui04@gmail.com>
2025-07-10 13:33:05 -0400

    added cahce to location request

--- PokeAPI/request.go
+++ PokeAPI/request.go
@@ -3,11 +3,16 @@ package pokeapi
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"time"
 
+	pokecache "github.com/saifullah605/Pokedex/pokecache"
 )
 
-const BASELOCATIONS string = "https://pokeapi.co/api/v2/location-area/"
+const BASELOCATIONS string = "https://pokeapi.co/api/v2/location-area/?offset=0&limit=20"
+
+var locationCache = pokecache.NewCache(10 * time.Minute)
 
 var ErrorBackPage = errors.New("you are on the backpage")
 var ErrorPageFront = errors.New("you are on the front page")
@@ -44,12 +49,22 @@ func generalRequest[T any](url string, buffer *T) error {
 
 func MapRequest() (locations, error) {
 	if locationsData.locationsEndpoint != "" {
+
+		if cacheData, hit := locationCache.Get(locationsData.locationsEndpoint); hit {
+			locCached := cacheData.(locations)
+			fmt.Println("CACHED")
+			locationsData.locationsEndpoint = *locCached.Next
+			locationsData.prevLocationsEndpoint = locCached.Prev
+			return locCached, nil
+		}
+
 		var data locations
 		if err := generalRequest(locationsData.locationsEndpoint, &data); err != nil {
 			return locations{}, err
 		}
 
 		locationsData.prevLocationsEndpoint = data.Prev
+		locationCache.Add(locationsData.locationsEndpoint, data)
 
 		if data.Next == nil {
 			locationsData.locationsEndpoint = ""
@@ -66,15 +81,32 @@ func MapRequest() (locations, error) {
 }
 
 func PrevMapRequest() (locations, error) {
+
 	if locationsData.prevLocationsEndpoint == nil {
 		return locations{}, ErrorPageFront
 	}
 
-	var data locations
-	if err := generalRequest(*locationsData.prevLocationsEndpoint, &data); err != nil {
-		return locations{}, err
+	if cacheData, hit := locationCache.Get(*locationsData.prevLocationsEndpoint); hit {
+		locCached := cacheData.(locations)
+		fmt.Println("CACHED")
+		// Set locationsEndpoint to the NEXT page, not the current one
+		if locCached.Next != nil {
+			locationsData.locationsEndpoint = *locCached.Next
+		} else {
+			locationsData.locationsEndpoint = ""
+		}
+		locationsData.prevLocationsEndpoint = locCached.Prev
+		return locCached, nil
 	}
-	locationsData.locationsEndpoint = *locationsData.prevLocationsEndpoint
+
+	var data locations
+	locationCache.Add(*locationsData.prevLocationsEndpoint, data)
+	// Set locationsEndpoint to the NEXT page, not the current one
+	if data.Next != nil {
+		locationsData.locationsEndpoint = *data.Next
+	} else {
+		locationsData.locationsEndpoint = ""
+	}
 	locationsData.prevLocationsEndpoint = data.Prev
 	return data, nil
 
